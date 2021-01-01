85a071db239b5dc0e71824cb97d34b99d3792cae
shuvo151dey <48350056+shuvo151dey@users.noreply.github.com>
2021-01-01 12:02:02 +0530

    Update Certi_F.js

--- src/component/Certi_F.js
+++ src/component/Certi_F.js
@@ -1,35 +1,36 @@
-import React, { useRef } from 'react'
+import React, { useRef,useEffect } from 'react'
 import certificate2 from './certificate2.jpg'
 
 const Landing4 = () => {
 
-let ctx ;
-const ref =  useRef();
-if(window!="undefined"){
- const image = new Image(certificate2);
-  ctx = ref.current.getContext('2d');
-  
-  image.src = certificate2;
-  console.log(certificate2)
 
-  image.addEventListener('load', () => {
-    debugger
-    console.log('image is loaded2');
-    ctx.drawImage(image, 0, 0, ref.width, ref.height)
-    ctx.font = '15px monotype corsiva'
-    ctx.fillStyle = '#001'
-    ctx.fillText('Vishal patel' , 300, 230)
-    ctx.fillText('IIT_KGP' , 110, 245)
-    ctx.fillText('TFT' , 400, 245)
-    
-  }); 
-}
-// const Download = (dwn) =>{
-//     dwn.href = toDataURL('image/jpg')
-//     dwn.download = 'SF_participation'
-    
-// }
-console.log(ref)
+const ref =  useRef();
+const ref2 = useRef();
+
+useEffect(() => {
+  
+  const ctx = ref.current.getContext('2d');
+
+  const image = new Image(certificate2);
+ 
+   image.src = certificate2;
+  //  console.log(image)
+  console.log(ctx)
+   image.addEventListener('load', () => {
+      
+      //  console.log('image is loaded2');
+       ctx.drawImage(image, 0, 0, ref.current.width, ref.current.height)
+       ctx.font = '15px monotype corsiva'
+       ctx.fillStyle = '#001'
+       ctx.fillText('Vishal patel' , 300, 230)
+       ctx.fillText('IIT_KGP' , 110, 245)
+       ctx.fillText('TFT' , 400, 245)
+   
+     }); 
+    ref2.current.href = ref.current.toDataURL("image/png")
+  },[])
+  
+
   return (
     <div>
     <canvas id="canvas"
@@ -38,7 +39,7 @@ console.log(ref)
     ref={ ref }
   
     />
-    <a href='#' id="dwn"  >
+    <a ref={ref2}  download="certificate.png">
       Download_1
     </a>
   
